62a6c30d325523984580be85824a7d6ba0a02249
MehrabR <Mehrab.Rahman@Revature.com>
2017-08-09 15:31:27 -0400

    ServletDemo v2

--- ServletDemo/src/main/java/com/revature/servlets/HelloServlet.java
+++ ServletDemo/src/main/java/com/revature/servlets/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.revature.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,13 +14,29 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, 
 			HttpServletResponse resp) 
 					throws ServletException, IOException {
-		System.out.println("We did get");
+		//System.out.println("We did get");
+		PrintWriter out = resp.getWriter();
+		out.println("<p>Did the Get</p>");
+		out.println("<a href=\"index.html\">Go Back</a>");
+		
+		String n1 = req.getParameter("n1");
+		String n2 = req.getParameter("n2");
+		
+		int result = Integer.parseInt(n1) + Integer.parseInt(n2);
+		out.println("<p>Sum of two numbers = " + result + "</p>");
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, 
 			HttpServletResponse resp) 
 					throws ServletException, IOException {
-		System.out.println("We did get");
+		//System.out.println("We did post");
+		PrintWriter out = resp.getWriter();
+		out.println("<p>Did the Post</p>");
+		out.println("<a href=\"index.html\">Go Back</a>");
+		String n1 = req.getParameter("fName");
+		String n2 = req.getParameter("lName");
+		String fullname = n1 + " " + n2;
+		out.println("<p>Full Name: " + fullname + "</p>");
 	}
 }
