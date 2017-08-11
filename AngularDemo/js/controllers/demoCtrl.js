1f046e203950d4a4dfa36d8e3204a405c2872b90
MehrabR <Mehrab.Rahman@Revature.com>
2017-08-11 14:03:15 -0400

    AngularDemo

--- AngularDemo/js/controllers/demoCtrl.js
+++ AngularDemo/js/controllers/demoCtrl.js
@@ -1,3 +1,14 @@
 angular.module('demoApp').controller('demoCtrl', function demoCtrl($scope) {
-  //$scope.firstName = "Mehrab";
+  $scope.firstName = "Mehrab";
+  $scope.newFirstName = "Merv";
+  $scope.people = [{name:'Robert', state:'NC'}, {name:'Louis', state:'TX'}, {name:'Jesse', state:'NY'}];
+
+  $scope.changeName = function() {
+    $scope.firstName = $scope.newFirstName;
+  };
+
+  $scope.myFunction = function(myEvent) {
+    $scope.x = myEvent.clientX;
+    $scope.y = myEvent.clientY;
+  }
 }); 
