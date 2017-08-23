671ff41f65611916028666969ad3010959718c1a
MehrabR <Mehrab.Rahman@Revature.com>
2017-08-23 15:18:40 -0400

    SpringMVC

--- SpringMVCDemo/src/main/java/com/revature/controllers/HelloController.java
+++ SpringMVCDemo/src/main/java/com/revature/controllers/HelloController.java
@@ -6,11 +6,14 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.revature.model.User;
 
@@ -28,8 +31,8 @@ public class HelloController {
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public String login(String username, String password, HttpServletRequest req) {
-		User u = new User((String) req.getParameter("username"),(String) req.getParameter("password"));
-		
+		User u1 = new User((String) req.getParameter("username"),(String) req.getParameter("password"));
+		User u = new User(username, password);
 		if (u.getUsername().equals("admin") && u.getPassword().equals("admin")) {
 			return "redirect:home";
 		} else {
@@ -44,6 +47,25 @@ public class HelloController {
 		names.add("Kelvin");
 		names.add("Eleazar");
 		return ResponseEntity.ok(names);
+	}
+	
+	@RequestMapping(value="/user", method=RequestMethod.GET)
+	public ResponseEntity<Object> getUser() {
+		User a = new User("admin1", "admin1");
+		return ResponseEntity.ok(a);
+	}
+	
+	@RequestMapping(value="/hidden", method=RequestMethod.GET)
+	public String goSecret() {
+		return "secret";
+	}
+	
+	@RequestMapping(value="/{fname}/{lname}", method=RequestMethod.GET)
+	@ResponseBody
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<String> fullname(@PathVariable("fname") String fname, @PathVariable("lname") String lname) {
+		String output = fname + " " + lname;
+		return ResponseEntity.ok(output);
 	}
 
 }
